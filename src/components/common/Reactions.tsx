5f488028f5d5186754ad5bdf07c0897ab041cda4
sermain2 <ksm6430@naver.com>
2026-04-05 00:58:47 +0900

    feat: Reactions 컴포넌트 TypeScript 변환

---
+++ src/components/common/Reactions.tsx
@@ -0,0 +1,35 @@
+import styles from "./Reactions.module.scss";
+
+interface ReactionItem {
+  id: number;
+  emoji: string;
+  count: number;
+}
+
+interface ReactionsProps {
+  reactions: ReactionItem[];
+  page: string;
+}
+
+function Reactions({ reactions, page }: ReactionsProps) {
+  return (
+    <ol className={`${styles["reaction-box"]} ${styles[page]}`}>
+      {reactions?.length > 0 ? (
+        reactions.map((reaction) => (
+          <li key={reaction.id} className="reaction font-16-16-14">
+            <div className={styles["emoji-box"]}>
+              <div className="emoji">{reaction.emoji}</div>
+              <div className="count">{reaction.count}</div>
+            </div>
+          </li>
+        ))
+      ) : (
+        <div className={styles["non-reacted"]}>
+          관심을 기다리는 중...
+        </div>
+      )}
+    </ol>
+  );
+}
+
+export default Reactions;
